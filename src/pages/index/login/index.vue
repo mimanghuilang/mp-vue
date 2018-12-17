3c843ec6ac195d3cd99429d050abefbd03f668a2
mimanghuilang <1217302591@qq.com>
2018-12-17 18:04:38 +0800

    试着写登录页面

--- src/pages/index/login/index.vue
+++ src/pages/index/login/index.vue
@@ -1,53 +1,64 @@
 <template>
-  <div>
-    11111111111111111111
-    12324342
-    312442
-    31
-    2241
-    12123
-    332
-    424
-    42
-    42
-    <ul class="container log-list">
-      <li v-for="(log, index) in logs" :class="{ red: aa }" :key="index" class="log-item">
-        <card :text="(index + 1) + ' . ' + log"></card>
-      </li>
-    </ul>
+  <div class="login-content">
+    <div class="company-logo">
+      <img class="image" src="" alt="企业logo">
+    </div>
+    <div class="login-box">
+      <div class="phone-box">
+        <input type="text" class="phone-input">
+      </div>
+      <div class="password-box">
+        <input type="password" class="password-input">
+        <button>获取验证码</button>
+      </div>
+      <div class="button-box">
+        <Button>登录</Button>
+      </div>
+
+    </div>
+
+
   </div>
 </template>
 
 <script>
-import { formatTime } from '@/utils/index'
-import card from '@/components/card'
+  import { formatTime } from '@/utils/index';
+  import card from '@/components/card';
 
-export default {
-  components: {
-    card
-  },
+  console.log(formatTime);
+  export default {
+    components: {
+      card
+    },
 
-  data () {
-    return {
-      logs: []
+    data () {
+      return {};
+    },
+
+    created () {
     }
-  },
-
-  created () {
-    const logs = (wx.getStorageSync('logs') || [])
-    this.logs = logs.map(log => formatTime(new Date(log)))
-  }
-}
+  };
 </script>
 
-<style>
-.log-list {
-  display: flex;
-  flex-direction: column;
-  padding: 40rpx;
-}
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .login-content {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    .company-logo {
+      width: 100%;
+      height: 200rpx;
+      text-align: center;
+      margin-top: 100rpx;
+      .image{
+        width: 200rpx;
+        height: 200rpx;
+        background-color: green;
+        -webkit-border-radius: 50%;
+        -moz-border-radius: 50%;
+        border-radius: 50%;
+      }
+    }
+  }
 
-.log-item {
-  margin: 10rpx;
-}
 </style>
