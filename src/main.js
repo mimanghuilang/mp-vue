e57002ef05c24167b8d240f2f8852050010a3987
mimanghuilang <1217302591@qq.com>
2018-12-17 13:57:45 +0800

    修改样式引入

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App'
 
-Vue.config.productionTip = false
-App.mpType = 'app'
+// 引入样式文件
+import '../static/css/app.css'
 
-const app = new Vue(App)
-app.$mount()
+Vue.config.productionTip = false;
+
+App.mpType = 'app';
+
+const app = new Vue(App);
+
+app.$mount();
